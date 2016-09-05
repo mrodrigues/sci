41b41c33ae17f8cd95ea0be1cd7ada30e1faa2cb
Pedro dos Santos Pereira <pedrodkp@gmail.com>
2016-09-05 20:12:57 -0300

    Compatibility with postgree 2

--- db/migrate/20160802162015_change_text_to_longtext.rb
+++ db/migrate/20160802162015_change_text_to_longtext.rb
@@ -1,6 +1,6 @@
 class ChangeTextToLongtext < ActiveRecord::Migration
   def up
-    change_column :articles, :body, :text, :limit => 4294967295
+    change_column :articles, :body, :text, :limit => 1073741823
   end
 
   def down
